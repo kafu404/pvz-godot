7505e589e7fa9e34eceb208844824f2b439df771
Nguyen Huong Anh <huonganh.nguyen@ceff.ch>
2026-06-30 15:23:19 +0200

    plante vs skizo

---
+++ framework/classes/celulle.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+func _on_mouse_entered() -> void:
+	
+
+func _on_mouse_exited() -> void:
+	pass # Replace with function body.
+	
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	pass # Replace with function body.
---
+++ framework/classes/global/game_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var current_world : Node2D
+var cursor_plante : Node2D
+
+var plan
+func select_plant(plant : plantBase):
+	pass
+	
+func refresh_cellule(pos : Vector2i):
+	pass
